cf33cc9cf389a980d038d3ce5454be49b0772859
sdziuda <szymondziuda95@gmail.com>
2023-04-24 23:32:04 +0200

    Added first receiver implementation
    Currently on one thread with plans of adding the second one (one to read, one to write)

--- sikradio-receiver.cpp
+++ sikradio-receiver.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include "common.h"
 
-#define DEFAULT_PORT "28422"
 #define DEFAULT_BSIZE 65536
 
 using namespace std;
@@ -50,35 +49,63 @@ int main(int argc, char *argv[]) {
 
     char *addr = (char *) address_input.c_str();
     char *port = (char *) port_input.c_str();
-    char buffer[bsize];
+    byte_t buffer[bsize];
     memset(buffer, 0, sizeof(buffer));
 
     uint16_t port_num = read_port(port);
     int socket_fd = bind_socket(port_num);
+    struct sockaddr_in source_address = get_address(addr, 0);
     struct sockaddr_in sender_address{};
     size_t read_length;
+    size_t psize = 0;
+    uint64_t session_id = 0;
+    uint64_t byte_0 = 0;
 
     do {
-        read_length = read_message(socket_fd, &sender_address, &buffer, sizeof(buffer));
+        read_length = read_message(socket_fd, &sender_address, buffer, sizeof(buffer));
         if (read_length > 0) {
-            cerr << "Received " << read_length << " bytes from "
-                 << inet_ntoa(sender_address.sin_addr) << ":"
-                 << ntohs(sender_address.sin_port) << endl;
-            size_t psize = read_length - sizeof(uint64_t) * 2;
+            if (strcmp(inet_ntoa(source_address.sin_addr),
+                       inet_ntoa(sender_address.sin_addr)) != 0) {
+                cerr << "Received packet from wrong address, skipping" << endl;
+                continue;
+            }
 
-            uint64_t session_id;
-            memcpy(&session_id, buffer, sizeof(uint64_t));
-            session_id = ntohl(session_id);
-            cerr << "session_id: " << session_id << endl;
+            uint64_t tmp_id;
+            memcpy(&tmp_id, buffer, sizeof(uint64_t));
+            tmp_id = ntohl(tmp_id);
 
             uint64_t first_byte_num;
             memcpy(&first_byte_num, buffer + sizeof(uint64_t), sizeof(uint64_t));
             first_byte_num = ntohl(first_byte_num);
-            cerr << "first_byte_num: " << first_byte_num << endl;
 
-            cerr << "psize: " << psize << endl;
+            if (session_id == 0) {
+                session_id = tmp_id;
+                byte_0 = first_byte_num;
+                psize = read_length - sizeof(uint64_t) * 2;
+            } else if (session_id > tmp_id) {
+                continue;
+            } else if (session_id < tmp_id) {
+                session_id = 0;
+                memset(buffer, 0, sizeof(buffer));
+                continue;
+            } else {
+                size_t header_len = sizeof(uint64_t) * 2 + psize;
+                size_t buf_position = first_byte_num - byte_0 + header_len;
+                memcpy(buffer + buf_position, buffer + sizeof(uint64_t) * 2, psize);
+                for (size_t i = sizeof(uint64_t) * 2; i < buf_position; i += psize) {
+                    if (buffer[i] == 0) {
+                        cerr << "MISSING: BEFORE " << first_byte_num << " EXPECTED "
+                             << first_byte_num - (buf_position - i) << endl;
+                    }
+                }
 
-            cout.write(buffer + sizeof(uint64_t) * 2, psize);
+                if (buf_position >= (3 * bsize) / 4) {
+                    size_t write_size = buf_position + psize - header_len;
+                    fwrite(buffer + header_len, sizeof(byte_t), write_size, stdout);
+                    byte_0 = first_byte_num + psize;
+                    memset(buffer, 0, sizeof(buffer));
+                }
+            }
         }
     } while (read_length > 0);
 
