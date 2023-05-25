9343aafac373e6fa0bbc5194addb1ab897f366f7
sdziuda <szymondziuda95@gmail.com>
2023-05-25 18:50:16 +0200

    Improved rexmit thread
    One thread for the whole time now, not a new one each time

--- sikradio-sender.cpp
+++ sikradio-sender.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <cstring>
 #include <sys/socket.h>
-#include <sys/time.h>
 #include <ctime>
 #include <cstdio>
 #include <thread>
 #include <mutex>
+#include <atomic>
+#include <condition_variable>
 #include "common.h"
 
 #define DEFAULT_PORT "28422"
@@ -17,10 +18,15 @@
 using std::string;
 namespace po = boost::program_options;
 
-bool control_finished = false;
-std::mutex send_mutex;
+std::atomic_bool finished = false;
+bool stop_sleeping = false;
+std::mutex buffer_mutex;
+std::mutex packets_set_mutex;
+std::mutex sleep_mutex;
+std::condition_variable sleep_cv;
 byte_t *retransmission_buffer = nullptr;
 std::set<uint64_t> retransmission_set;
+std::set<uint64_t> packets_to_resend;
 
 namespace {
     void help_and_exit(const string &name) {
@@ -119,8 +125,8 @@ namespace {
         response[index++] = '\n';
     }
 
-    // checks and parses rexmit requests
-    void parse_rexmit(const string &message, std::set<uint64_t> &rexmit) {
+    // checks and parses requests into packets_to_resend set
+    void parse_rexmit(const string &message) {
         if (message[message.size() - 1] != '\n') {
             return;
         }
@@ -133,6 +139,7 @@ namespace {
             index++;
         }
 
+        std::unique_lock<std::mutex> lock(packets_set_mutex, std::defer_lock);
         index = REXMIT_HEADER_SIZE;
         while (index < message.size()) {
             size_t comma = message.find(',', index);
@@ -140,7 +147,9 @@ namespace {
                 comma = message.size();
             }
             uint64_t number = stoull(message.substr(index, comma - index));
-            rexmit.insert(number);
+            lock.lock();
+            packets_to_resend.insert(number);
+            lock.unlock();
             index = comma + 1;
         }
     }
@@ -158,6 +167,7 @@ namespace {
 
     // saves the input data for later in case of retransmission
     void save_for_retransmission(uint64_t num, size_t psize, size_t fsize, byte_t *buffer) {
+        std::unique_lock<std::mutex> lock(buffer_mutex);
         if (retransmission_set.size() >= fsize / psize) {
             retransmission_set.erase(retransmission_set.begin());
             retransmission_set.insert(num);
@@ -167,12 +177,14 @@ namespace {
 
         size_t buf_position = get_position_in_buffer(num, psize, fsize);
         memcpy(retransmission_buffer + buf_position, buffer, psize);
+        lock.unlock();
     }
 
     // function for the thread handling retransmission
-    void resend(std::set<uint64_t> rexmit, uint64_t session_id, size_t psize,
-                size_t fsize, uint16_t port_num, char *addr) {
-        std::unique_lock<std::mutex> lock(send_mutex, std::defer_lock);
+    void resend(uint64_t net_session_id, size_t psize, size_t fsize, uint16_t port_num,
+                char *addr, uint64_t rtime) {
+        std::unique_lock<std::mutex> lock(buffer_mutex, std::defer_lock);
+        std::unique_lock<std::mutex> set_lock(packets_set_mutex, std::defer_lock);
 
         // setting up socket and address
         int socket_fd = open_udp_socket();
@@ -191,28 +203,54 @@ namespace {
                                sizeof(ip_mreq)));
 
         byte_t buffer[psize + 2 * sizeof(uint64_t)];
-        memcpy(buffer, &session_id, sizeof(uint64_t));
+        memcpy(buffer, &net_session_id, sizeof(uint64_t));
 
-        while (!rexmit.empty()) {
-            uint64_t number = *rexmit.begin();
-            rexmit.erase(rexmit.begin());
+        while (!finished) {
+            // setting the start timestamp
+            auto start = std::chrono::system_clock::now();
 
-            uint64_t net_number = htobe64(number);
-            memcpy(buffer + sizeof(uint64_t), &net_number, sizeof(uint64_t));
+            // moving packets to be retransmitted to rexmit set
+            set_lock.lock();
+            std::set<uint64_t> rexmit(packets_to_resend);
+            packets_to_resend.clear();
+            set_lock.unlock();
 
-            lock.lock();
-            if (retransmission_set.find(number) == retransmission_set.end()) {
+            // retransmitting packets
+            while (!rexmit.empty()) {
+                uint64_t number = *rexmit.begin();
+                rexmit.erase(rexmit.begin());
+
+                uint64_t net_number = htobe64(number);
+                memcpy(buffer + sizeof(uint64_t), &net_number, sizeof(uint64_t));
+
+                lock.lock();
+                if (retransmission_set.find(number) == retransmission_set.end()) {
+                    lock.unlock();
+                    continue;
+                }
+
+                size_t id = get_position_in_buffer(number, psize, fsize);
+                std::cerr << "retransmit: " << id << " ";
+                memcpy(buffer + sizeof(uint64_t) * 2, retransmission_buffer + id, psize);
+                std::cerr.write((char *) (buffer + sizeof(uint64_t) * 2), psize);
                 lock.unlock();
+
+                send_message(socket_fd, &send_address, buffer, psize + sizeof(uint64_t) * 2);
+            }
+
+            // calculating the remaining time of this retransmission period
+            auto stop = std::chrono::system_clock::now();
+            auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+            uint64_t sleep = rtime - duration.count();
+            if (sleep <= 0) {
                 continue;
             }
 
-            size_t index = get_position_in_buffer(number, psize, fsize);
-            std::cerr << "retransmit: " << index << " ";
-            memcpy(buffer + 2 * sizeof(uint64_t), retransmission_buffer + index, psize);
-            std::cerr.write((char *) (buffer + 2 * sizeof(uint64_t)), psize);
-            lock.unlock();
-
-            send_message(socket_fd, &send_address, buffer, psize + 2 * sizeof(uint64_t));
+            // sleeping for the remaining rtime, unless finished
+            std::unique_lock<std::mutex> sleep_lock(sleep_mutex);
+            sleep_cv.wait_for(sleep_lock, std::chrono::milliseconds(sleep), [] {
+                return stop_sleeping;
+            });
         }
 
         CHECK_ERRNO(setsockopt(socket_fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, &ip_mreq,
@@ -221,9 +259,7 @@ namespace {
     }
 
     // function for the thread listening for control messages
-    void listen_control(char *addr, char *port, uint16_t port_num, char *name,
-                        uint64_t rtime, uint64_t session_id, size_t psize,
-                        size_t fsize, uint16_t send_port_num) {
+    void listen_control(char *addr, char *port, uint16_t port_num, char *name) {
         byte_t buffer[UDP_MAX_SIZE];
         memset(buffer, 0, sizeof(buffer));
 
@@ -232,28 +268,7 @@ namespace {
 
         struct sockaddr_in sender_address{};
 
-        std::set<uint64_t> packets_to_resend;
-        struct timeval last{};
-        gettimeofday(&last, nullptr);
-        std::thread retransmission;
-        bool r_started = false;
-
-        while (!control_finished) {
-            struct timeval now{}, diff{};
-            gettimeofday(&now, nullptr);
-            timersub(&now, &last, &diff);
-            uint64_t time_passed = diff.tv_sec * 1000 + diff.tv_usec / 1000;
-            if (time_passed >= rtime || !r_started) {
-                last = now;
-                if (r_started) {
-                    retransmission.join();
-                }
-                retransmission = std::thread(resend, packets_to_resend, session_id,
-                                             psize, fsize, send_port_num, addr);
-                r_started = true;
-                packets_to_resend.clear();
-            }
-
+        while (!finished) {
             size_t read_length = read_message(socket_fd, &sender_address, buffer,
                                               sizeof(buffer));
             if (read_length == 0) {
@@ -268,13 +283,10 @@ namespace {
                 create_lookup_reply(reply, addr, port, name);
                 send_message(socket_fd, &sender_address, &reply, sizeof(reply));
             } else if (strncmp(message.c_str(), REXMIT_HEADER, REXMIT_HEADER_SIZE) == 0) {
-                parse_rexmit(message, packets_to_resend);
+                parse_rexmit(message);
             }
         }
 
-        if (r_started) {
-            retransmission.join();
-        }
         CHECK_ERRNO(close(socket_fd));
     }
 }
@@ -327,10 +339,11 @@ int main(int argc, char *argv[]) {
         fatal("invalid fsize");
     }
 
-    std::unique_lock<std::mutex> lock(send_mutex, std::defer_lock);
+    std::unique_lock<std::mutex> lock(buffer_mutex, std::defer_lock);
     std::thread control_thread(listen_control, addr, c_port, c_port_num,
-                               (char *) name.c_str(), rtime, net_session_id,
-                               psize, fsize, d_port_num);
+                               (char *) name.c_str());
+    std::thread retransmission_thread(resend, net_session_id, psize, fsize, d_port_num,
+                                      addr, rtime);
 
     // the main loop, reading data from input and sending it to the multicast
     while (true) {
@@ -345,17 +358,21 @@ int main(int argc, char *argv[]) {
 
         memcpy(buffer + sizeof(uint64_t) * 2, read_buffer, psize);
 
-        lock.lock();
         save_for_retransmission(first_byte_num, psize, fsize, read_buffer);
-        lock.unlock();
 
         send_message(socket_fd, &send_address, &buffer, sizeof(buffer));
 
         first_byte_num += psize;
     }
 
-    control_finished = true;
+    finished = true;
     control_thread.join();
+    {
+        std::lock_guard<std::mutex> sleep_lock(sleep_mutex);
+        stop_sleeping = true;
+        sleep_cv.notify_one();
+    }
+    retransmission_thread.join();
     delete[] retransmission_buffer;
 
     CHECK_ERRNO(setsockopt(socket_fd, IPPROTO_IP, IP_DROP_MEMBERSHIP, &ip_mreq,
